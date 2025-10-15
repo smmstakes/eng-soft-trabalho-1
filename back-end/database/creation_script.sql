ffe0e6be33eae11d0e78f5e385a9b093aa0ef091
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-10-15 14:52:21 -0300

    refactor: ajustando o script e o banco de dados

--- back-end/database/creation_script.sql
+++ back-end/database/creation_script.sql
@@ -16,8 +16,14 @@ CREATE TABLE IF NOT EXISTS "user_story" (
         "titulo_user_story"     TEXT,
         "objetivo"      TEXT,
         "beneficio"     TEXT,
+        "nivel_story" TEXT,
         PRIMARY KEY("id_user_story" AUTOINCREMENT),
-        FOREIGN KEY("id_projeto") REFERENCES "projeto"("id_projeto")
+        FOREIGN KEY("id_projeto") REFERENCES "projeto"("id_projeto"),
+        FOREIGN KEY("nivel_story") REFERENCES "prioridade_story"("nivel_story")
+);
+CREATE TABLE IF NOT EXISTS "prioridade_story"(
+        "nivel_story" TEXT,
+        PRIMARY KEY("nivel_story")
 );
 CREATE TABLE IF NOT EXISTS "task" (
         "id_task"       INTEGER,
@@ -25,9 +31,15 @@ CREATE TABLE IF NOT EXISTS "task" (
         "cpf"   TEXT,
         "nome_estado"   TEXT,
         "descricao_task"        TEXT,
+        "nivel_task" TEXT,
         PRIMARY KEY("id_task" AUTOINCREMENT),
         FOREIGN KEY("id_sprint") REFERENCES "sprint"("id_sprint"),
-        FOREIGN KEY("nome_estado") REFERENCES "estado_task"("nome_estado")
+        FOREIGN KEY("nome_estado") REFERENCES "estado_task"("nome_estado"),
+        FOREIGN KEY("nivel_task") REFERENCES "prioridade_task"("nivel_task")
+);
+CREATE TABLE IF NOT EXISTS "prioridade_task"(
+        "nivel_task" TEXT,
+        PRIMARY KEY("nivel_task")
 );
 CREATE TABLE IF NOT EXISTS "estado_task" (
         "nome_estado"   TEXT,
@@ -35,12 +47,11 @@ CREATE TABLE IF NOT EXISTS "estado_task" (
 );
 CREATE TABLE IF NOT EXISTS "sprint" (
         "id_sprint"     INTEGER,
-        "id_user_story" INTEGER,
         "meta"  TEXT,
-        "prazo" DATE,
+        "inicio" DATE,
+        "termino" DATE,
         "revisao_sprint"        TEXT,
-        PRIMARY KEY("id_sprint" AUTOINCREMENT),
-        FOREIGN KEY("id_user_story") REFERENCES "user_story"("id_user_story")
+        PRIMARY KEY("id_sprint" AUTOINCREMENT)
 );
 CREATE TABLE IF NOT EXISTS "usuario_projeto" (
         "cpf"   TEXT,
@@ -58,4 +69,11 @@ CREATE TABLE IF NOT EXISTS "projeto" (
         "cpf"   TEXT,
         PRIMARY KEY("id_projeto" AUTOINCREMENT),
         FOREIGN KEY("cpf") REFERENCES "usuario"("cpf")
+);
+CREATE TABLE IF NOT EXISTS "user_story_sprint" (
+        "id_user_story" INTEGER,
+        "id_sprint" INTEGER, 
+        PRIMARY KEY("id_user_story", "id_sprint"),
+        FOREIGN KEY("id_user_story") REFERENCES "user_story"("id_user_story"),
+        FOREIGN KEY("id_sprint") REFERENCES "sprint"("id_sprint")
 );
