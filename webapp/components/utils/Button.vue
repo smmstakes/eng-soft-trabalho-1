3bf09c6e0d8917b7f9b14849d4bc7c759f076e91
smmstakes <matheusds014@gmail.com>
2025-10-19 15:05:51 -0300

    chore: troca components para pasta utils

--- webapp/components/utils/Button.vue
+++ webapp/components/utils/Button.vue
@@ -7,6 +7,8 @@
 <script setup>
 import { ref } from 'vue'
 
+const hover = ref(false);
+
 const props = defineProps({
     text: {
         type: String,
@@ -17,42 +19,40 @@ const props = defineProps({
         default: 'black',
         validator: value => ['black', 'white'].includes(value)
     }
-})
-
-const hover = ref(false)
+});
 </script>
 
 <style scoped>
 .mode-button {
-    font-weight: 400;
-    height: 40px;
-    padding: 0px 30px;
-    border: none;
-    border-radius: 6px;
-    transition: 0.5s;
-    cursor: pointer;
+	font-weight: 400;
+	height: 40px;
+	padding: 0px 30px;
+	border: none;
+	border-radius: 6px;
+	transition: 0.5s;
+	cursor: pointer;
 }
 
 .mode-button.black {
-    background-color: #171717;
-    color: white;
+	background-color: #171717;
+	color: white;
 }
 
 .mode-button.black:hover {
-    background-color: #fff;
-    color: black;
-    border: 1px solid #D4D4D4;
+	background-color: #fff;
+	color: black;
+	border: 1px solid #D4D4D4;
 }
 
 .mode-button.white {
-    background-color: #fff;
-    color: black;
-    border: 1px solid #D4D4D4;
+	background-color: #fff;
+	color: black;
+	border: 1px solid #D4D4D4;
 }
 
 .mode-button.white:hover {
-    background-color: #171717;
-    color: white;
-    border: none;
+	background-color: #171717;
+	color: white;
+	border: none;
 }
 </style>
---
+++ webapp/components/utils/TopBar.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="top-bar">
+		<div class="app-title">
+			<img src="/logo.svg" class="logo" alt="Logo do sistema">
+			<h3>Sistema Scrum</h3>
+		</div>
+
+		<NuxtLink to="/configuration" class="profile">
+			<img :src="profileImage" class="profile-image" alt="Perfil">
+		</NuxtLink>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+const profileImage = ref('') // TODO: Receber foto vinda da API depois
+
+</script>
+
+<style scoped>
+.top-bar {
+	width: 100%;
+	height: 65px;
+	display: flex;
+
+	align-items: center;
+	justify-content: space-between;
+	border-bottom: 2.5px solid #E5E5E5;
+}
+
+.top-bar .logo {
+	margin: 0px 25px;
+}
+
+.top-bar h3 {
+	font-weight: 400;
+}
+
+.top-bar .app-title {
+	display: flex;
+}
+
+.top-bar .profile {
+	width: 40px;
+	height: 40px;
+	background-color: #4caf50;
+	border-radius: 50%;
+
+	margin-right: 25px;
+	justify-self: flex-start;
+}
+</style>
