45e62135a41ed1dbffb219504d4dd02813df5cb4
suyanne <suyannesara17@gmail.com>
2025-11-04 16:55:14 -0300

    feat: adiciona modais de criacao e ingresso em projeto

--- webapp/pages/projects.vue
+++ webapp/pages/projects.vue
@@ -2,20 +2,62 @@
     <div class="container">
         <TopBar />
         <Header class="header" title="Meus Projetos" description="Gerencie e acompanhe seus projetos de eventos">
-            <Button text="➜ Ingressar em um Projeto" mode="black" />
-            <Button text="+ Novo Projeto" mode="black" />
+            <Button  @click="showJoinModal = true" text="➜ Ingressar em um Projeto" mode="black" />
+            <Button  @click="showCreateModal = true" text="+ Novo Projeto" mode="black" />
         </Header>
 
         <div class="projects-grid">
             <ProjectCard v-for="project in projects" :key="project.projectId" :icon="getIcon(project.name)"
                 :title="project.name" :description="project.description" :project-id="project.projectId" />
         </div>
+
+        <Modal title="Criar Projeto" v-model:show="showCreateModal">
+            <form @submit.prevent="submitForm">
+                <div class="form-group">
+                    <label>Título do Projeto *</label>
+                    <input type="text" placeholder="Digite seu nome" required />
+                </div>
+
+                <div class="form-group">
+                    <label>Descrição do Projeto *</label>
+                    <textarea type="email" placeholder="Digite seu email" required />
+                </div>
+
+                <div class="form-group">
+                    <label>Senha de ingresso ao projeto *</label>
+                    <input type="password" placeholder="Digite a senha de ingresso ao projeto" required />
+                </div>
+
+                <Button text="Criar" mode="black" />
+            </form>
+        </Modal>
+
+        <Modal title="Ingressar em um Projeto" v-model:show="showJoinModal">
+            <form @submit.prevent="submitJoinForm">
+                <div class="form-group">
+                    <label>
+                        ID do Projeto
+                        <Tooltip text="Os membros da sua equipe utilizarão essa senha para ingressar no projeto." />
+                    </label>
+                    <input type="text" placeholder="Digite o ID do projeto" required />
+                </div>
+
+                <div class="form-group">
+                    <label>Senha de ingresso ao projeto *</label>
+                    <input type="password" placeholder="Digite a senha de ingresso ao projeto" required />
+                </div>
+
+                <Button text="Ingressar" mode="black" />
+            </form>
+        </Modal>
     </div>
 </template>
 <script setup>
 import { ref, onMounted } from 'vue'
 
 const projects = ref([])
+const showCreateModal = ref(false)
+const showJoinModal = ref(false)
 
 onMounted(async () => {
     const res = await fetch('/api/projects')
@@ -32,11 +74,12 @@ function getIcon(name) {
 
 <style scoped>
 .projects-grid {
-	display: grid;
-	grid-template-columns: repeat(4, 1fr); /* 4 colunas fixas */
-	gap: 20px;
-	margin-top: 24px;
-	padding: 12px 5%;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    /* 4 colunas fixas */
+    gap: 20px;
+    margin-top: 24px;
+    padding: 12px 5%;
 
 }
 
