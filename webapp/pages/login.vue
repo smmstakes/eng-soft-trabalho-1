6c4a980f180b9ee88915c972f0a3b0f4545c1ba2
suyanne <suyannesara17@gmail.com>
2025-11-11 08:57:26 -0300

    feat: criar front telas login e cadastro

---
+++ webapp/pages/login.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="auth-container">
+        <h1>Login</h1>
+        <form @submit.prevent="submitLogin">
+            <div class="form-group">
+                <label>
+                    E-mail: *
+                </label>
+                <input type="email" placeholder="Digite o e-mail cadastrado na plataforma" required />
+            </div>
+
+            <div class="form-group">
+                <label>Senha: *</label>
+                <input type="password" placeholder="Digite a senha de ingresso a plataforma" required />
+            </div>
+
+            <Button class="auth-join-button" text="Entrar" mode="black" />
+        </form>
+
+        <span class="auth-instruction-text">Não tem uma conta?
+            <NuxtLink to="/register">
+                <List /> Cadastre-se
+            </NuxtLink>
+        </span>
+    </div>
+
+</template>
+
+
+<script>
+</script>
---
+++ webapp/pages/register.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="auth-container">
+        <h1>Cadastro</h1>
+        <form @submit.prevent="submitLogin">
+            <div class="form-group">
+                <label>
+                    CPF: *
+                </label>
+                <input type="text" placeholder="Digite seu cpf" required />
+            </div>
+
+            <div class="form-group">
+                <label>
+                    Nome: *
+                </label>
+                <input type="text" placeholder="Digite seu cpf" required />
+            </div>
+
+            <div class="form-group">
+                <label>
+                    E-mail: *
+                </label>
+                <input type="email" placeholder="Digite o e-mail que deseja cadastrar" required />
+            </div>
+
+            <div class="form-group">
+                <label>Senha: *</label>
+                <input type="password" placeholder="Digite a senha que deseja criar" required />
+            </div>
+
+            <Button class="auth-join-button" text="Cadastrar" mode="black" />
+        </form>
+
+        <span class="auth-instruction-text">Já tem uma conta? Faça
+            <NuxtLink to="/login">
+                <List />Login
+            </NuxtLink>
+        </span>
+    </div>
+
+</template>
+
+
+<script>
+</script>
